7ac30fd5db738ab95868935ea5d501ea77804342
Genilson Alves <171091849+genilssom@users.noreply.github.com>
2025-08-27 20:58:35 -0300

    Create run_music+iSS.sh

---
+++ run_music+iSS.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#Clean up all files (if any)
+
+rm music_run/*.dat
+rm iss_run/results/*.dat
+rm iss_run/*.dat
+
+#run MUSIC
+
+cp codes/music_code/build/src/MUSIChydro music_run/
+
+
+(
+  cd music_run
+  ./MUSIChydro ../input/music_input
+  cp surface_eps_0.18.dat ../iss_run/results/surface.dat
+)
+
+cp input/music_input iss_run/results/
+
+#Run iSS
+
+cp codes/iss_code/build/src/iSS.e iss_run/
+cp input/iSS_parameters.dat iss_run/
+
+
+(
+  cd codes/iss_code/iSS_tables/deltaf_tables
+  bash download_newRTA.sh
+)
+
+(
+  cd iss_run
+  ./iSS.e
+)
+
