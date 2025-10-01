dd063830f9e7592211d57214868d1556b8edc88b
genilssom <genilsonc01@gmail.com>
2025-10-01 18:23:47 -0300

    Add script for hadronic afterburner test setup and execution

---
+++ hadronic_afterburner_test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+rm ~/hadronic_afterburner_toolkit/*.oscar
+rm ~/hadronic_afterburner_toolkit/*.bin
+rm ~/hadronic_afterburner_toolkit/particle_lists
+
+( 
+    cd smash_run/data/0
+    mv particle_lists.oscar ~/hadronic_afterburner_toolkit/
+)
+
+#(
+#    cd smash_run/data/0
+#    mv particles_oscar2013_extended.bin  ~/hadronic_afterburner_toolkit/results/particles_binary.bin
+#)
+
+( 
+    cd ~/hadronic_afterburner_toolkit
+    ./convert_to_binary_SMASH.e particle_lists.oscar
+    gunzip particle_lists.gz
+    mv particle_lists results/particles_list.dat
+    ./hadronic_afterburner_tools.e
+)
+
+#(
+#    cd ~/hadronic_afterburner_toolkit
+#    ./hadronic_afterburner_tools.e
+#
+#)
