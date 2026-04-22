81a0b8a7b53e580024164321e595d8ad088dac40
genilssom <genilsonc01@gmail.com>
2026-04-22 13:27:42 -0300

    Update branch names in get_music+iss.sh for MUSIC and iSS repositories

--- codes/get_music+iss.sh
+++ codes/get_music+iss.sh
@@ -3,19 +3,20 @@
 
 # download MUSIC
 rm -rf music_code
-git clone https://github.com/MUSIC-fluid/MUSIC.git -b resummedTransportCoeff music_code
+git clone https://github.com/MUSIC-fluid/MUSIC.git -b XSCAPE music_code
+(cd music_code; git checkout cfcc26455450588961e81139045dc0eef387437c)
 rm -rf music_code/.git
 
 # download iSS
 rm -rf iss_code
-git clone https://github.com/chunshen1987/iSS.git -b 4DEoS iss_code
+git clone https://github.com/chunshen1987/iSS.git -b XSCAPE iss_code
+(cd iss_code; git checkout d242555306930f813881caca500f0e6f82036b2e)
 rm -rf  iss_code/.git
 
 #download SMASH
 
 rm -rf smash_code
-git clone https://github.com/smash-transport/smash.git -b main smash_code
+git clone https://github.com/smash-transport/smash.git -b SMASH-3.2.2 smash_code
 rm -rf smash_code/.git
-
 
 
