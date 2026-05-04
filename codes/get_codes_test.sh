9c918231aa648376f61c570c4b97a1f35effdf4a
Genilson Alves <171091849+genilssom@users.noreply.github.com>
2026-05-04 10:53:50 -0300

    Update git clone command for MUSIC repository

--- codes/get_codes_test.sh
+++ codes/get_codes_test.sh
@@ -3,7 +3,8 @@
 
 # download MUSIC
 rm -rf music_code
-git clone https://github.com/luizafperin/MUSIC.git -b main music_code
+git clone --depth=3 https://github.com/luizafperin/MUSIC -b main MUSIC_code
+(cd MUSIC_code; git checkout e41c72a68b6cb13680d4a0421b06653f978b572a)
 rm -rf music_code/.git
 
 # download iSS (newRTA branch + OSCAR2013 patch)
